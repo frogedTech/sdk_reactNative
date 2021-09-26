9b5c6233fbeb00b41e55cdd98c9cc7f398f8e254
Angel Romero <angel@froged.com>
2021-09-26 11:51:29 +0200

    Froged SDK

--- App.tsx
+++ App.tsx
@@ -1,21 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, Button } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { BehaviorSubject } from 'rxjs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+
+    const [ frogedOpen, setFrogedOpen ] = React.useState(false);
+    const [ frogedAlerted, setFrogedAlerted ] = React.useState(false);
+    const [ frogedUserId, setFrogedUserId ] = React.useState('');
+    let webviewRef: any = null;
+
+    /**
+     * Send commands to webview
+     * @param command Command name
+     * @param params Optional command parameters
+     */
+    const runCommand = (command: string, params?: any) => {
+        const message = { command, params };
+        webviewRef.postMessage(JSON.stringify(message));
+    }
+
+    const openFroged = (requestId?: string) => {
+        setFrogedOpen(true);
+        runCommand('open', { requestId: requestId?? null })
+    }
+
+    const openFrogedWithRoute = (route: 'home' | 'inbox' | 'contact' | 'documents' | 'updates') => {
+        setFrogedOpen(true);
+        runCommand('open', { route });
+    }
+
+    const closeFroged = () => {
+        setFrogedOpen(false);
+        runCommand('close')
+    }
+
+    const set = (dataToSet: any) => {
+        runCommand('set', { dataToSet });
+    }
+
+    const track = (eventName: string, meta?: any) => {
+        runCommand('track', { eventName, meta });
+    }
+
+    const showAutomessage = (idAutomessage: string) => {
+        runCommand('automessage', { idAutomessage });
+    }
+
+    const showDocument = (idDocument: string) => {
+        runCommand('document', { idDocument });
+    }
+
+    const logout = () => {
+        runCommand('logout');
+    }
+
+    /**
+     * Listen commands from webview
+     */
+    const messagesListener = new BehaviorSubject<{ command: string, params: any } | null>(null);
+    messagesListener.subscribe(message => {
+        if (!message) return;
+        const { command, params } = message;
+        switch (command) {
+            case 'froged_closeRequest':
+                closeFroged();
+                break;
+            case 'froged_openRequest':
+                const requestId = params.requestId;
+                openFroged(requestId);
+                break;
+            case 'froged_hook_onConnect':
+                break;
+            case 'froged_hook_onAuth':
+                const { auth } = params;
+                setFrogedUserId(auth.userId);
+                break;
+            case 'froged_hook_onInboxStatusUpdate':
+                const { unreadMessagesCount } = params;
+                setFrogedAlerted(unreadMessagesCount !== 0);
+                break;
+        }
+    });
+
+    //   UI
+    return (
+        <>
+            <Text/>
+            { !frogedOpen && frogedAlerted && <Button color="#ff0000" title="Open FROGED" onPress={() => openFroged()} /> }
+            { !frogedOpen && !frogedAlerted && <Button color="#000000" title="Open FROGED" onPress={() => openFroged()} /> }
+            <Button title="Track (eventName = 'localhost')" onPress={() => track('localhost', { url: 'ejemplo' })} />
+            { !frogedUserId && <Button title="Login as Pedro" onPress={() => set({ userId: 'user_pedro', name: 'Pedro' })} /> }
+            { !frogedUserId && <Button title="Login as Juan" onPress={() => set({ userId: 'user_juan', name: 'Juan' })} /> }
+            { !!frogedUserId && <Button title={`Logout (${ frogedUserId })` } onPress={() => logout()} /> }
+            <WebView 
+                onMessage={(e) => {
+                    const data = JSON.parse(e.nativeEvent.data);
+                    let command = data.command;
+                    let params = data.params;
+                    messagesListener.next({ command, params });
+                }}
+                containerStyle={ frogedOpen ? {} : { display: 'none' }}
+                source={{ uri: 'http://1eee-185-137-52-134.ngrok.io/webview/sdk-wv.html?slug=IIFqhs' }}
+                ref={ref => (webviewRef = ref)}
+            ></WebView>
+      </>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
